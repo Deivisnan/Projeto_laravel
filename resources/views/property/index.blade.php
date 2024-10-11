7659e10de27f16705d602c292dab1ad8c0992df8
Deivisnan da Silva Bispo <bispodeivisnan@gmail.com>
2024-10-11 18:10:33 -0300

    inicio de estilização do formulário

---
+++ resources/views/property/index.blade.php
@@ -0,0 +1,37 @@
+@extends('property.master')
+
+@section('content')
+    <h1 class="titulo">Listagem de Imóveis</h1>
+    @if (!empty($properties))
+        <table class="property-table">
+            <thead>
+            <tr>
+                <th>Título</th>
+                <th>Valor de Locação</th>
+                <th>Valor de Compra</th>
+                <th>Ações</th>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach ($properties as $property)
+                <tr>
+                    <td>{{ $property->title }}</td>
+                    <td>R$ {{ number_format($property->rental_price, 2, ',', '.') }}</td>
+                    <td>R$ {{ number_format($property->sale_price, 2, ',', '.') }}</td>
+                    <td>
+                        <a class="action-button view" href="{{ url('/imoveis/' . $property->name) }}">Ver mais</a>
+                        <a class="action-button edit" href="{{ url('/imoveis/editar/' . $property->name) }}">Editar</a>
+                        <form action="{{ url('/imoveis/remover/' . $property->name) }}" method="POST" style="display:inline;">
+                            {{ csrf_field() }}
+                            {{ method_field('DELETE') }}
+                            <button type="submit" class="action-button remove">Remover</button>
+                        </form>
+                    </td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+    @else
+        <p>Nenhum imóvel cadastrado.</p>
+    @endif
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/imoveis','App\Http\Controllers\PropertyController@index');
-Route::get('/imoveis/novo','App\Http\Controllers\PropertyController@create');
-Route::post('/imoveis/store','App\Http\Controllers\PropertyController@store');
+Route::get('/imoveis', 'App\Http\Controllers\PropertyController@index')->name('imoveis.index');
+Route::get('/imoveis/novo', 'App\Http\Controllers\PropertyController@create')->name('imoveis.create');
+Route::post('/imoveis/store', 'App\Http\Controllers\PropertyController@store')->name('imoveis.store');
+Route::get('/imoveis/{name}', 'App\Http\Controllers\PropertyController@show')->name('imoveis.show');
+Route::get('/imoveis/editar/{name}', 'App\Http\Controllers\PropertyController@edit')->name('imoveis.edit');
+Route::put('/imoveis/update/{name}', 'App\Http\Controllers\PropertyController@update')->name('imoveis.update');
+Route::delete('/imoveis/remover/{name}', 'App\Http\Controllers\PropertyController@destroy')->name('imoveis.destroy');
